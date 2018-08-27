285d4fb9f6975584633685abfa895b9ab0685ed2
Dhruv Mishra <ubiquitiousdm@gmail.com>
2018-08-27 21:58:59 +0200

    fixing import again

--- conver_to_pcm.py
+++ conver_to_pcm.py
@@ -6,5 +6,5 @@ from tqdm import tqdm
 
 files = glob.glob(sys.argv[1])
 for file in tqdm(files):
-    sr, in_audio = scipy.io.wavfile.read(file)
+    sr, in_audio = wavfile.read(file)
     scipy.io.wavfile.write(file, sr, (in_audio*300).astype(np.int16))
