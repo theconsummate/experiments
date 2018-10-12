990034fa5daa960a1c14ff77e8293fd12aa6ad47
Dhruv Mishra <ubiquitiousdm@gmail.com>
2018-10-12 18:32:36 +0200

    spectrogram final config

--- converter/train.py
+++ converter/train.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import os
 from scipy import signal
 from scipy.io import wavfile
@@ -6,15 +7,24 @@ import numpy as np
 
 def load_data(root):
     speakers = ["VCC2SF1", "VCC2SF2", "VCC2TF1", "VCC2TF2"]
-    file = os.path.join(root, speakers[0], "10002.wav")
+    target = os.path.join(root, speakers[-1], "10001.wav")
+    twav, _ = librosa.load(target)
+    
+    for spk in speakers[:-1]:
+        file = os.path.join(root, spk, "10001.wav")
 
-    sample_rate, samples = wavfile.read(file)
-    print(samples)
-    frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate)
-    print(spectrogram.shape)
+        wav, _ = librosa.load(file)
+        rate = len(wav)/len(twav)
+        # print(rate)
+        wav_norm = librosa.effects.time_stretch(wav, rate)
+        wav_norm = librosa.util.fix_length(wav_norm, len(twav), mode='edge')
 
-    D = librosa.stft(y=np.array([float(x) for x in samples]), n_fft=512, hop_length=80, win_length=400)
-    mag = np.abs(D)
+
+
+        D = librosa.stft(y=wav_norm, n_fft=512, hop_length=80, win_length=400)
+        mag = np.abs(D).T
+        print(mag.shape)
+    D = librosa.stft(y=twav, n_fft=512, hop_length=80, win_length=400)
     print(mag.shape)
 
 def main():
