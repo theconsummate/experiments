58e4f1eff6f5c9edff6efad29337a37838c5f097
Dhruv Mishra <ubiquitiousdm@gmail.com>
2018-12-10 17:54:45 +0100

    adding support for multiple merges

--- merge_json_files.py
+++ merge_json_files.py
@@ -1,21 +1,27 @@
 import sys
 import json
-# usage python merge_json_files.py fullpath1 fullpath2 fulloutputpath
+# usage python merge_json_files.py fulloutputpath fullpath1 fullpath2 ... 
 
-f1 = open(sys.argv[1])
-f2 = open(sys.argv[2])
+# f1 = open(sys.argv[1])
+# f2 = open(sys.argv[2])
 
-j1 = json.load(f1)["utts"]
-j2 = json.load(f2)["utts"]
+# j1 = json.load(f1)["utts"]
+# j2 = json.load(f2)["utts"]
 
 out_utts = {}
 
-for key in j1.keys():
-    out_utts[key+"_1"] = j1[key]
+for i, file in enumerate(sys.argv[2:]):
+    f = open(file)
+    j = json.load(f)["utts"]
+    for key in j.keys():
+        out_utts[key+"_" + i] = j[key]
 
-for key in j2.keys():
-    out_utts[key+"_2"] = j2[key]
+# for key in j1.keys():
+#     out_utts[key+"_1"] = j1[key]
+
+# for key in j2.keys():
+#     out_utts[key+"_2"] = j2[key]
 
 
-f3 = open(sys.argv[3], 'w')
+f3 = open(sys.argv[1], 'w')
 json.dump({"utts":out_utts}, f3, indent=4)
