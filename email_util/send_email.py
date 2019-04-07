601eef38f7ae32adf1c821a664b84a854a908b49
Dhruv Mishra <ubiquitiousdm@gmail.com>
2019-04-07 19:33:21 +0200

    adding option to send email to multiple people

--- email_util/send_email.py
+++ email_util/send_email.py
@@ -11,30 +11,33 @@ def send(subject, message, config_file_path = "config.json"):
     msg = MIMEMultipart()
 
     # message = "Thank you"
-    
+
     # setup the parameters of the message
     password = params["hlrs-password"]
     msg['From'] = params["hlrs-email"]
-    msg['To'] = params["gmail"]
+    if isinstance(params["gmail"], str):
+        msg['To'] = params["gmail"]
+    else:
+        msg['To'] = ", ".join(params["gmail"])
     msg['Subject'] = subject
-    
+
     # add in the message body
     msg.attach(MIMEText(message, 'plain'))
-    
+
     #create server
     server = smtplib.SMTP_SSL('mail.hlrs.de:465')
-    
+
     # server.starttls()
-    
+
     # Login Credentials for sending the mail
     server.login(msg['From'], password)
-    
-    
+
+
     # send the message via the server.
     server.sendmail(msg['From'], msg['To'], msg.as_string())
-    
+
     server.quit()
-    
+
     print("successfully sent email to %s:" % (msg['To']))
 
 
