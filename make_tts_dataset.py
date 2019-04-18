9319959e7504c350dc70403ead7ca951cbf36855
Dhruv Mishra <ubiquitiousdm@gmail.com>
2019-04-18 18:56:01 +0200

    fixing shuffle

--- make_tts_dataset.py
+++ make_tts_dataset.py
@@ -10,17 +10,18 @@ dirname = fname.replace("txt.done.data", "") + "wav/"
 
 def process_dataset(arr, outfile):
     fo = open(outfile, 'w')
-    for line in arr[:5]:
+    for line in arr:
         """
         an example of a single line.
         line = '( arctic_a0001 "Author of the danger trail, Philip Steels, etc." )\n'
         """
         wav, text = line.strip()[2:-2].replace('"', "").split(" ", 1)
-        fo.write(dirname+wav+".wav|" + text+"\n")
+        fo.write(dirname+wav+".wav|" + text.strip()+"\n")
     fo.close()
 
 f = open(fname, 'r')
-contents = random.shuffle(f.readlines())
+contents = f.readlines()
+random.shuffle(contents)
 f.close()
 
 size = len(contents)
